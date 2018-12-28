a2922cdfcadb6ec5dda4c7e23174862a6e6979e2
Grady Day <gday@stanford.edu>
2018-12-28 07:03:34 -0800

    Added pathway lists

---
+++ R/MSIGDB.pathways.R
@@ -0,0 +1,12 @@
+#' MSIGDB pathway lists
+#'
+#' @docType data
+#'
+#' @usage data(MSIGDB.pathways)
+#'
+#' @references Liberzon et al. The Molecular Signatures Database (MSigDB) hallmark gene set collection. Cell Syst. 2015 Dec 23;1(6):417-425. 
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/26771021}{PubMed})
+#'
+#' @examples
+#' data(MSIGDB.pathways)
+"MSIGDB.pathways"

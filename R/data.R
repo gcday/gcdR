d5524eb2f2be6fa5967efc8a1a827f31dc99221a
Grady Day <gday@stanford.edu>
2019-01-15 22:40:52 -0800

    Added mouse cell cycle gene list

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Mouse cell cycle genes
+#'
+#' @docType data
+#'
+#' @usage data(mouse.cc.genes)
+#'
+#' @references Liberzon et al. The Molecular Signatures Database (MSigDB) hallmark gene set collection. Cell Syst. 2015 Dec 23;1(6):417-425. 
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/26771021}{PubMed})
+#'
+#' @examples
+#' data(mouse.cc.genes)
+"mouse.cc.genes"
